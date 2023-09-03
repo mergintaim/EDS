1fe86f84df8f6d84b738d74b57d1e8e0bf0232e5
mergintaim <77895854+mergintaim@users.noreply.github.com>
2023-09-03 15:58:33 +0200

    Phaseaccu fertig

--- Labor4/Labor4.srcs/sim_1/new/tb_Counter.vhd
+++ Labor4/Labor4.srcs/sim_1/new/tb_Counter.vhd
@@ -18,7 +18,6 @@
 -- 
 ----------------------------------------------------------------------------------
 
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -33,51 +32,51 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity tb_Counter is
     Generic(
-        g_wordlength : natural := 15;
-        g_dimension_of_Vectors: natural := 15
+        g_wordlength           : natural := 15;
+        g_dimension_of_Vectors : natural := 15
     );
 end tb_Counter;
 
-architecture Behavioral of tb_Counter is
+architecture Test of tb_Counter is
     component Counter
-    generic(
-        g_maxnumber  : natural;
-        g_wordlength : natural
-    );
-    port(
-        i_Clk       : in  std_logic;
-        i_reset     : in  std_logic;
-        i_increment : in  std_logic_vector(0 to g_wordlength-1);
-        o_output    : out std_logic_vector(0 to g_wordlength-1)
-    );
-end component Counter;
+        generic(
+            g_maxnumber  : natural;
+            g_wordlength : natural
+        );
+        port(
+            i_Clk       : in  std_logic;
+            i_reset     : in  std_logic;
+            i_increment : in  std_logic_vector(0 to g_wordlength - 1);
+            o_output    : out std_logic_vector(0 to g_wordlength - 1)
+        );
+    end component Counter;
 
-signal i_Clk : std_logic;
-signal i_reset : std_logic;
-signal i_sel_phasestep : std_logic_vector(0 to g_dimension_of_Vectors-1);
-signal o_phase_output :  std_logic_vector(0 to g_dimension_of_Vectors-1);
+    signal i_Clk           : std_logic := '0';
+    signal i_reset         : std_logic := '0';
+    signal i_sel_phasestep : std_logic_vector(0 to g_dimension_of_Vectors - 1);
+    signal o_phase_output  : std_logic_vector(0 to g_dimension_of_Vectors - 1);
 
-signal s_phasestep : integer := 0;
-signal s_phaseoutput : integer;
-
+    signal s_phasestep   : integer := 0;
+    signal s_phaseoutput : integer;
 
 begin
 
-cnt: Counter
-    generic map(
-        g_maxnumber  => 32768,
-        g_wordlength => g_dimension_of_Vectors
-    )
-    port map(
-        i_Clk       => i_Clk,
-        i_reset     => i_reset,
-        i_increment => i_sel_phasestep,
-        o_output    => o_phase_output
-    );
+    cnt : Counter
+        generic map(
+            g_maxnumber  => 32768,
+            g_wordlength => g_dimension_of_Vectors
+        )
+        port map(
+            i_Clk       => i_Clk,
+            i_reset     => i_reset,
+            i_increment => i_sel_phasestep,
+            o_output    => o_phase_output
+        );
 
-    s_phaseoutput <= to_integer(unsigned(o_phase_output));
-    i_sel_phasestep <= std_logic_vector(to_unsigned(s_phasestep,g_dimension_of_Vectors));
+    s_phaseoutput   <= to_integer(unsigned(o_phase_output));
+    i_sel_phasestep <= std_logic_vector(to_unsigned(s_phasestep, g_dimension_of_Vectors));
+    s_phasestep     <= 15;
 
-    i_Clk    <= not i_Clk after 10 ns;
-
-end Behavioral;
+    i_Clk   <= not i_Clk after 10 ns;
+    i_reset <= '1' after 2 ns, '0' after 3 ns;
+end Test;
