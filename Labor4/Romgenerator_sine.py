89e27a3ef9222021abe1f5623bb328c361b1eb6b
mergintaim <77895854+mergintaim@users.noreply.github.com>
2023-09-03 20:05:08 +0200

    fixed small things in romgenerator

--- Labor4/Romgenerator_sine.py
+++ Labor4/Romgenerator_sine.py
@@ -2,8 +2,6 @@
 # Generate Sinus Rom File with arbitrary precision
 import os
 import sys
-
-
 import math as m
 import numpy as np
 import pathlib as pl
@@ -11,12 +9,11 @@ import pathlib as pl
 #initilization
 PATH_TO_ROMFILEFOLDER = __file__
 FILEEXTENSION = '.coe'
-FILENAME = str(pl.Path(PATH_TO_ROMFILEFOLDER).parent.resolve().absolute()) + '\\' + 'sine_ROM' + FILEEXTENSION
 INITSTRING = 'memory_initialization_radix=10;\nmemory_initialization_vector=\n'
-number_of_datapoints = 32768
-bitwidth_of_counter = m.ceil(m.log2(number_of_datapoints))
+NUMBER_OF_DATAPOINTS = 32768
+bitwidth_of_counter = m.ceil(m.log2(NUMBER_OF_DATAPOINTS))
+filename = str(pl.Path(PATH_TO_ROMFILEFOLDER).parent.resolve().absolute()) + '\\' + 'sine_ROM' +'_maxnum_' + str(NUMBER_OF_DATAPOINTS) + "_" + str(bitwidth_of_counter) +'_bit' + FILEEXTENSION
 
-##
 def yes_or_no(question):
     while "the answer is invalid":
         reply = str(input(question+' (y/n): ')).lower().strip()
@@ -27,21 +24,22 @@ def yes_or_no(question):
 
 def main():
     #create sin values
-    x_elements = list(range(0,number_of_datapoints,1))
-    x_elements_floats = [(element * 2 * m.pi)/number_of_datapoints for element in x_elements]
+    x_elements = list(range(0,NUMBER_OF_DATAPOINTS,1))
+    x_elements_floats = [(element * 2 * m.pi)/NUMBER_OF_DATAPOINTS for element in x_elements]
     sine = np.sin(x_elements_floats)
-    sine_ints = [m.floor((element+1)*(number_of_datapoints/2)) for element in sine]
+    sine_ints = [m.floor((element+1)*(NUMBER_OF_DATAPOINTS/2)) for element in sine]
     #generate sine string
     sinestring = ",\n".join(map(str,sine_ints))
     ## Loop to fill file
     try:
-        romfile = open(FILENAME,"x")
+        romfile = open(filename,"x")
     except Exception as e:
         overwrite = yes_or_no("Do you want to overwrite the old file? ")
         if not overwrite: print("Dann halt nicht du Pisser!");return
-    romfile = open(FILENAME,"w")
+    romfile = open(filename,"w")
 
     romfile.write(INITSTRING + sinestring + ";")
+    romfile.close
     
 if __name__ == "__main__":
     main()
