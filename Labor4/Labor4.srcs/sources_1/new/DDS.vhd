4dfbb297143661b5faf46addaedf76567fe328c7
mergintaim <77895854+mergintaim@users.noreply.github.com>
2023-09-10 12:06:38 +0200

    jan labert scheiße

--- Labor4/Labor4.srcs/sources_1/new/DDS.vhd
+++ Labor4/Labor4.srcs/sources_1/new/DDS.vhd
@@ -23,7 +23,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
